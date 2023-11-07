8116dab8eb07204ba0517404e367376955df2983
ayoubLems <aayyoouubb-lleemm@oulook.fr>
2023-11-07 17:04:18 +0100

    k

--- index.php
+++ index.php
@@ -27,61 +27,7 @@ if(isset($_POST) & !empty($_POST)){ //vérifier si le formulaire de soumission a
 ?>
 <?php require('templates/header.php') ?> <!--inclure le fichier header.php qui contient le code HTML pour la section 'head'-->
 
-<select name="choix">
 
-	<optgroup label="Ressource inerte">
-		<option value="11">- Mélanges bitumineux (sans goudron)</option>
-		<option value="12">-   Terres non polluées (hors terre végétale)</option>
-		<option value="13">-	Béton</option>
-		<option value="14">-	Pierr</option>
-		<option value="15">-	Tuiles et ardoises</option>
-		<option value="16">-	Briques</option>
-		<option value="17">-	Carrelages et faïences</option>
-		<option value="18">-	Mélange d’inertes listés ci-dessus sans DND</option>
-		<option value="19">-	Autres inertes</option>
-	</optgroup>
-
-	<optgroup label="Ressource non dangereuse non inerte">
-	<option value="2">-	Plâtre</option>
-	<option value="12">-	Plaques et carreaux</option>
-	<option value="13">-	Enduit et support inerte</option>
-	<option value="14">-	Bois</option>
-	<option value="15">-	Bois A (emballages, palettes)</option>
-	<option value="16">-	Bois B (peints, meubles)</option>
-	<option value="17">o	Cuivre</option>
-	<option value="18">o	Aluminium</option>
-	<option value="19">o	Ferreux</option>
-	<option value="12">o	Zinc</option>
-	<option value="13">o	Autres métaux non ferreux</option>
-	<option value="14">o	PVC souple</option>
-	<option value="15">o	PVC rigide</option>
-	<option value="16">o	PP</option>
-	<option value="16">o	PE</option>
-	<option value="16">o	Autres plastiques</option>
-	<option value="16">o	Moquette</option>
-	<option value="16">o	Linoléum</option>
-	<option value="16">o	Autres revêtements de sol</option>
-	<option value="16">o	Panneaux sandwich</option>
-	<option value="16">o	Complexes plâtre + isolant</option>
-	<option value="16">o	Complexe d'étanchéité sans goudron</option>
-	<option value="16">o	Autres matériaux complexes</option>
-	<option value="16">-	Fenêtres et autres ouvertures vitrées : bois/alu/pvc et simple vitrage ou double vitrage</option>
-	<option value="16">-	Mélange de DND listés ci-dessus</option>
-	<option value="16">-	Végétaux</option>
-	<option value="16">-	Terre végétale</option>
-	<option value="16">-	Autres matériaux ou déchets non dangereux</option>
-	</optgroup>
-
-	<optgroup label="Ressource d’équipement">
-	<option value="2">Équipements sanitaires (lavabos, éviers, WC…)</option>
-	<option value="16"></option>
-	<option value="16"></option>
-	<option value="16"></option>
-	<option value="16"></option>
-	</optgroup>
-
-
-	</select>
 
 <div class="d-flex">
 	<img src="img/materiaux.jpeg" class="hotelimg">
